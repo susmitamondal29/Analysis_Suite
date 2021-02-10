4ae1cbb194c1709755c54b8bc88890fdfa4d8668
Dylan Teague <dteague@wisc.edu>
2021-02-10 05:48:18 -0600

    Fix groups used for plotting
    Will need to genericize some of the names so they work across years,
    e.g. not including generator in group name

--- data/python/PlotGroups/ThreeTop.py
+++ data/python/PlotGroups/ThreeTop.py
@@ -1,64 +1,39 @@
 # -*- coding: utf-8 -*-
 info ={
-    "all": {
-        "Members": [
-            "tttj", "tttw",
-            "4top2016",
-            "ttw", "ttz", "tth2nonbb",
-            "ttg_dilep", "wzg", "wwg",
-            "zg", "wg", "ttg_lepfromT", "ttg_lepfromTbar", "ggh2zz",
-            "ttww", "ttzz", "ttwz", "ttzh", "tthh", "ttwh",
-            "wwz", "wzz", "www", "zzz",
-            "wz3lnu_mg5amcnlo", "zz4l_powheg", "vh2nonbb", "ww_doubleScatter",
-            "DYm10-50", "DYm50","st_twll", "tzq", "ttbar", "wjets", "wpwpjj_ewk",
-        ]
-    },
-
     "xg": {
-        "Style": "fill-indigo", 
         "Name": "X+\gamma", 
         "Members": [
             "wzg",
 	        "wwg",
-            "ttg_lepfromT",
-	        "ttg_lepfromTbar",
+            "ttg_hadronic",
+	        "ttg_singleLept",
 	        "ttg_dilep",
-	        # "zg",
-            # "wg"
+	        "zg",
+            "wg",
+            "tg"
         ]
     },
+
+    "tttt": {
+        "Name": r"t\bar{t}t\bar{t}",
+        "Members": ["tttt",]
+    },
+
     "ttw": {
-        "Style": "fill-darkgreen", 
         "Name": r"t\bar{t}W", 
-        "Members": [
-            "ttw"
-        ]
+        "Members": ["ttw"]
     },
     "ttz": {
-        "Style": "fill-mediumseagreen", 
         "Name": r"t\bar{t}Z/\gamma*", 
-        "Members": [
-            "ttz"
-        ]
+        "Members": ["ttz", "ttz_m1-10"]
     },
     "tth": {
-        "Style": "fill-slategray", 
         "Name": r"t\bar{t}H", 
-        "Members": [
-            "tth2nonbb"
-        ]
+        "Members": ["tth"]
     },
-    "ttX": {
-        "Name": r"t\bar{t}H",
-        "Members": [
-            "ttw",
-            "ttz",
-            "tth2nonbb"
-        ]
-    },
+
     "ttXY": {
-        "Style": "fill-cornflowerblue", 
-        "Name": r"t\bar{t}VV", 
+        "Name": r"t\bar{t}VV",
         "Members": [
             "ttww",
 	        "ttwz",
@@ -68,20 +43,19 @@ info ={
 	        "ttwh"
         ]
     },
+
     "vv": {
-        "Style": "fill-hotpink", 
-        "Name": "vv", 
+        "Name": "vv",
         "Members": [
             "vh2nonbb",
             "zz4l_powheg",
             "wpwpjj_ewk",
             "ww_doubleScatter",
-            "wz3lnu_mg5amcnlo",
+            "wz2lnu_powheg",
         ],
     },
     "vvv": {
-        "Style": "fill-hotpink", 
-        "Name": "vv", 
+        "Name": "vv",
         "Members": [
             "wwz",
             "wzz",
@@ -89,62 +63,15 @@ info ={
             "zzz",
         ],
     },
+
     "ttt": {
-        "Style": "fill-hotpink",
         "Name": "ttt",
-        "Members": [
-            "tttj",
-	        "tttw"
-        ]
+        "Members": ["tttj", "tttw"]
     },
-    "4top": {
-        "Style": "fill-hotpink",
-        "Name": "ttt",
-        "Members": [
-            "tttj",
-	        "tttw"
-        ]
-    },    
-    "rare": {
-        "Style": "fill-hotpink", 
-        "Name": "Rare", 
-        "Members": [
-            "tttj",
-	        "tttw",
-            "tzq",
-            "st_twll",
-	        "ggh2zz",
-            "vh2nonbb",
-	        "zz4l_powheg",
-	        "wpwpjj_ewk",
-	        "ww_doubleScatter",
-	        "wz3lnu_mg5amcnlo",
-            "www",
-	        "wwz",
-	        "wzz",
-	        "zzz",
-        ]
-    },
-    "rare_no3top": {
-        "Style": "fill-hotpink", 
-        "Name": "Rare", 
-        "Members": [
-            "st_twll",
-	        "ggh2zz",
-            "vh2nonbb",
-	        "zz4l_powheg",
-	        "wpwpjj_ewk",
-	        "ww_doubleScatter",
-	        "wz3lnu_mg5amcnlo",
-            "www",
-	        "wwz",
-	        "wzz",
-	        "zzz",
-        ]
-    },
+
     "other": {
-        "Style": "fill-hotpink", 
-        "Name": "Nonprompt", 
+        "Style": "fill-hotpink",
+        "Name": "Nonprompt",
         "Members": [
             'ttbar',
             "DYm50",
@@ -152,50 +79,18 @@ info ={
             "wjets",
             "tzq",
             "ggh2zz",
-        ]
-    },
-    "tttt": {
-        "Style": "nofill-hotpink", 
-        "Name": r"t\bar{t}t\bar{t}",
-        "Members": [
-            "4top",
-        ]
-    },
-    "tttt_201X" : {
-        "Name" : "4top",
-        "Members" : [
-            "tttt2016",
+            "st_twll",
+            "ttjet"
         ]
     },
 
-    "ttt_201X" : {
-        "Name": "3top",
-        "Members" : [
-            "tttw2016",
-            "tttw2017",
-            "tttw2018",
-            "tttj2016",
-            "tttj2017",
-            "tttj2018",
-        ]
-    },
-    
-    "mvaTest" : {
-        "Members" : [
-            "tttt2016",
-            "tttw2016",
-            "tttw2017",
-            "tttw2018",
-            "tttj2016",
-            "tttj2017",
-            "tttj2018",
-        ]
-    },
-    "ttg": {
+    "ttX": {
+        "Composite": True,
+        "Name": r"t\bar{t}H",
         "Members": [
-            "ttg_dilep",
-            "ttg_lepfromTbar",
-            "ttg_lepfromT",
+            "ttw",
+            "ttz",
+            "tth"
         ]
-    }
+    },
 }
