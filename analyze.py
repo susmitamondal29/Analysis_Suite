9d127b42cb8517ca403d5aa32902894464b272b6
Dylan Teague <dteague@wisc.edu>
2021-06-03 07:43:23 -0500

    Update analyze script to new format

--- analyze.py
+++ analyze.py
@@ -6,68 +6,75 @@ ROOT.gROOT.SetBatch(True)
 ROOT.gROOT.ProcessLine( "gErrorIgnoreLevel = 1001;")
 
 
-def setInputs(inputs, list_THX=[]):
+def setInputs(inputs):
     root_inputs = ROOT.TList()
-    for th1 in list_THX:
-        root_inputs.Add(th1)
     for key, value in inputs.items():
-        root_inputs.Add(ROOT.TNamed(key, str(value)))
+        item = None
+        if isinstance(value, list):
+            item = ROOT.TList()
+            item.SetName(key)
+            for subitem in value:
+                item.Add(ROOT.TNamed(subitem, subitem))
+        else:
+            item = ROOT.TNamed(key, str(value))
+        root_inputs.Add(item)
     return root_inputs
 
 
-inputfile = os.getenv("INPUT")
-outputfile = os.getenv("OUTPUT")
+def getSumW(infiles):
+    runChain = ROOT.TChain()
+    sumweight = ROOT.TH1F("sumweight", "sumweight", 1, 0, 1)
+    for fname in infiles:
+        runChain.Add(f"{fname}/Runs")
+    runChain.Draw("0>>sumweight",  "genEventSumw")
+    return sumweight
 
-if os.getenv("INPUT") is None:
-    inputfile = "blah.in"
-if os.getenv("OUTPUT") is None:
-    outputfile = "output.root"
 
+if __name__ == "__main__":
+    inputfile = "blah.in" if (env := os.getenv("INPUT")) is None else env
+    outputfile = "output.root" if (env := os.getenv("OUTPUT")) is None else env
 
-files = list()
-with open(inputfile) as f:
-    for line in f:
-        files.append(line.strip())
+    files = list()
+    with open(inputfile) as f:
+        for line in f:
+            files.append(line.strip())
 
-print(files)
-sampleName = files[0].split('/')
-analysisName = sampleName[sampleName.index("user")+2]
+    print(files)
+    sampleName = files[0].split('/')
+    analysisName = sampleName[sampleName.index("user")+2]
 
-analysis, year, selection = analysisName.split("_")
-info = FileInfo(analysis=analysis, year=year, selection=selection)
+    analysis, year, selection = analysisName.split("_")
+    info = FileInfo(analysis=analysis, year=year, selection=selection)
 
-inputs = dict()
-groupName = info.get_group(sampleName)
-inputs["DAS_Name"] = sampleName
-inputs["Group"] = groupName
-inputs['Analysis'] = analysis
-inputs['Selection'] = selection
-inputs["Xsec"] = info.get_xsec(groupName)
-inputs["Year"] = year
+    # Setup inputs
+    inputs = dict()
+    groupName = info.get_group(sampleName)
+    inputs["DAS_Name"] = sampleName
+    inputs["Group"] = groupName
+    inputs['Analysis'] = analysis
+    inputs['Selection'] = selection
+    inputs["Xsec"] = info.get_xsec(groupName)
+    inputs["Year"] = year
+    inputs["Systematics"] = ["LHEReweight"]
+    inputs["isData"] = False
+    rInputs = setInputs(inputs)
 
-fChain = ROOT.TChain()
-for fname in files:
-    fChain.Add(f"{fname}/Events")
+    # Run Selection
+    fChain = ROOT.TChain()
+    for fname in files:
+        fChain.Add(f"{fname}/Events")
 
-selector = getattr(ROOT, "ThreeTop")()
-rInputs = setInputs(inputs)
-selector.SetInputList(rInputs)
-fChain.Process(selector, "")
+    selector = getattr(ROOT, "ThreeTop")()
+    selector.SetInputList(rInputs)
+    fChain.Process(selector, "")
+    sumweight = getSumW(files)
 
-## Output
-rOutput = ROOT.TFile(outputfile, "RECREATE")
-rOutput.cd()
-anaFolder = ROOT.gDirectory.mkdir(groupName)
-anaFolder.cd()
-for i in selector.GetOutputList():
-    i.Write()
-    
-runChain = ROOT.TChain()
-sumweight = ROOT.TH1F("sumweight", "sumweight", 1, 0, 1)
-for fname in files:
-    runChain.Add(f"{fname}/Runs")
-runChain.Draw("0>>sumweight",  "genEventSumw")
-anaFolder.cd()
-sumweight.Write()
+    ## Output
+    rOutput = ROOT.TFile(outputfile, "RECREATE")
+    anaFolder = rOutput.mkdir(groupName)
 
-rOutput.Close()
+    anaFolder.WriteObject(sumweight, "sumweight")
+    for i in selector.GetOutputList():
+        anaFolder.WriteObject(i, i.GetName())
+
+    rOutput.Close()
