41396d7d9922cebffbc56490f1139b3e539ef93b
Dylan Teague <dteague@wisc.edu>
2021-10-13 07:43:43 -0500

    Add option to delete old files in farmout script

--- scripts/farmoutJob.sh
+++ scripts/farmoutJob.sh
@@ -10,6 +10,13 @@ if [ "$#" -ne 1 ]; then
     exit 1
 else
     echo "Files will be saved to /hdfs/store/user/$USER/${analysis}_YEAR_$1"
+    echo "This function will delete the files in the above directories if the directory already exists"
+    echo "    If you wish to keep these files, backing up is necessary"
+    read -r -p "    Are you sure you wish to continue? [Y/n]" response
+    if ! [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
+        echo "y not read, exiting without running"
+        exit 0
+    fi
 fi
 
 outname=$1
@@ -22,9 +29,8 @@ for year in ${years}; do
         ./scripts/get_files_hdfs.sh $analysis_dir > $run_file
     fi
 
-    if [[ -d "/nfs_scratch/$USER/$analysis_dir-analyze" ]]; then
-        rm -r "/nfs_scratch/$USER/$analysis_dir-analyze"
-    fi
+    rm -rf "/nfs_scratch/$USER/$analysis_dir-analyze"
+    rm -rf "/hdfs/store/user/$USER/${analysis}_${year}_${outname}"
 
     farmoutAnalysisJobs "$analysis_dir" \
         --input-file-list=$run_file \
