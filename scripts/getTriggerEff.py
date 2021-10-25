144950ea550a37cae65b4db889372e36c9c62e70
Dylan Teague <dteague@wisc.edu>
2021-10-25 05:16:12 -0500

    Clean up trigger eff code so plots can be made

--- scripts/getTriggerEff.py
+++ scripts/getTriggerEff.py
@@ -4,77 +4,68 @@ import uproot4 as uproot
 import numpy as np
 from scipy.stats import beta
 import boost_histogram as bh
+import ROOT
+ROOT.gROOT.SetBatch(True)
 
 from analysis_suite.commons.histogram import Histogram
-from analysis_suite.data.inputs import plot_params
+from analysis_suite.commons.info import PlotInfo
+import analysis_suite.data.inputs as plot_params
 
 #filename ="result_2018.root"
 filename ="output.root"
 year = "all"
 chans = ["CHAN_MM", "CHAN_EM", "CHAN_ME", "CHAN_EE",]
-lumi = plot_params.lumi[year]*1000
+# lumi = plot_params.lumi[year]*1000
+lumi = 1000*PlotInfo.lumi[year]
 
 class TriggerEff:
-    def __init__(self):
-        self.hists = dict()
-        self.sumw2 = dict()
-        self.bins = None
+    def __init__(self, updir, scale):
+        self.pass_hist = updir["passTrigger"].to_boost()*scale
+        self.all_hist = updir["failTrigger"].to_boost()*scale + self.pass_hist
+        self.size = self.pass_hist.axes[1].size
+        self.axis = self.pass_hist.axes[1].edges
 
-    def add(self, val, err, scale):
-        for i, chan in enumerate(chans):
-            chanVal = scale*val[i+1][1:-1]
-            chanVal[-1] += scale*val[i+1][-1]
-            err2Val = (scale*val[i+1][1:-1])**2
-            err2Val[-1] += (scale*err[i+1][-1])**2
-            if chan not in self.hists:
-                self.hists[chan] = chanVal
-                self.sumw2[chan] = err2Val
-            else:
-                self.hists[chan] += chanVal
-                self.sumw2[chan] += err2Val
-                
-    def getSplit(self, chan):
-        cumval = np.cumsum(self.hists[chan][::-1])[::-1]
-        cumsumw2 = np.cumsum(self.sumw2[chan][::-1])[::-1]
-        return cumval, np.sqrt(cumsumw2)
+    def cumsum(self, hist):
+        return np.cumsum(hist[::-1])[::-1]
 
+    def bins(self, chan, hist):
+        a = np.arange(self.size)+1
+        b = self.cumsum(hist.view()[chan].value)
+        c = self.cumsum(hist.view()[chan].variance)
+        return np.dstack([a, b, c])[0]
 
-passTrigger = TriggerEff()
-allTrigger = TriggerEff()
-bins = None
+    def fill_th1(self, th1_pass, th1_all, chan):
+        for i, value, variance in self.bins(chan, self.pass_hist):
+            th1_pass.SetBinContent(int(i), value)
+            th1_pass.SetBinError(int(i), np.sqrt(variance))
+        for i, value, variance in self.bins(chan, self.all_hist):
+            th1_all.SetBinContent(int(i), value)
+            th1_all.SetBinError(int(i), np.sqrt(variance))
+
+# passTrigger = TriggerEff()
+# allTrigger = TriggerEff()
+# bins = None
+
+def get_tlist_var(tlist, varName):
+    for item in tlist:
+        if item.member('fName') == varName:
+            return item.member('fTitle')
+    return None
+
 with uproot.open(filename) as f:
     groups = [key.strip(";1") for key in f.keys() if "/" not in key]
     for group in groups:
-        xsec = float(f[group]["Xsec"].member("fTitle"))
+        xsec = float(get_tlist_var(f[group]["MetaData"], "Xsec"))
         sumw = sum(f[group]["sumweight"].values())
         scale = lumi*xsec/sumw
-        if bins is None:
-            bins = f[group]["passTrigger"].edges(1)[1:-1]
-        passVal, passErr = f[group]["passTrigger"].values_errors()
-        failVal, failErr = f[group]["failTrigger"].values_errors()
+        trig = TriggerEff(f[group], scale)
 
-        passTrigger.add(passVal, passErr, scale)
-        allTrigger.add(passVal, passErr, scale)
-        allTrigger.add(failVal, failErr, scale)
-
-
-nbins = len(bins) - 1
-import ROOT
-ROOT.gROOT.SetBatch(True)
-
-for chan in chans:
-    passTH1 = ROOT.TH1D("pass_{}".format(chan), "pass_{}".format(chan), nbins, bins)
-    pval, psumw = passTrigger.getSplit(chan)
-    allTH1 = ROOT.TH1D("all_{}".format(chan), "all_{}".format(chan), nbins, bins)
-    aval, asumw = allTrigger.getSplit(chan)
-
-    for b in range(nbins):
-        passTH1.SetBinContent(b+1, pval[b])
-        passTH1.SetBinError(b+1, psumw[b])
-        allTH1.SetBinContent(b+1, aval[b])
-        allTH1.SetBinError(b+1, asumw[b])
-    eff = ROOT.TEfficiency(passTH1, allTH1)
-    
-    can = ROOT.TCanvas("canvas", "Trigger Efficiency")
-    eff.Draw("ap")
-    can.SaveAs("eff_{}.pdf".format(chan))
+        nbins = trig.size
+        for nchan, chan in enumerate(chans):
+            passTH1 = ROOT.TH1D("pass_{}".format(chan), "pass_{}".format(chan), trig.size, trig.axis)
+            allTH1 = ROOT.TH1D("all_{}".format(chan), "all_{}".format(chan), trig.size, trig.axis)
+            trig.fill_th1(passTH1, allTH1, nchan)
+            eff = ROOT.TEfficiency(passTH1, allTH1)
+            can = ROOT.TCanvas("canvas", "Trigger Efficiency")
+            eff.Draw("ap")
+            can.SaveAs("eff_{}.pdf".format(chan))
