75d60264253df36bbd65e4f625aaf28c2e497db9
Dylan Teague <dteague@wisc.edu>
2022-07-14 11:03:29 -0500

    Move progress bar to its own class

---
+++ Analyzer/interface/Bar.h
@@ -0,0 +1,28 @@
+#ifndef BAR_H_
+#define BAR_H_
+
+#include <string>
+#include <iostream>
+
+class Bar {
+public:
+    void print_header();
+    void print_bar();
+    void print_trailer();
+    void set_total(size_t total) { total_events = total; }
+
+    void operator++(int) { n_proc_events++; }
+    size_t operator() () { return n_proc_events; }
+    void pass() { passed_events++; }
+
+private:
+
+    size_t passed_events = 0;
+    size_t total_events = 0;
+    size_t n_proc_events = 0;
+
+    const size_t barWidth  = 75;
+    size_t freq = 10000;
+};
+
+#endif // BAR_H_
---
+++ Analyzer/src/Bar.cc
@@ -0,0 +1,37 @@
+#include "analysis_suite/Analyzer/interface/Bar.h"
+
+#include <sstream>
+#include <cmath>
+
+void Bar::print_header()
+{
+    std::cout << "Total number of events: " << total_events << std::endl;
+}
+
+void Bar::print_bar()
+{
+    if (n_proc_events % freq != 0 && n_proc_events != total_events)
+        return;
+
+    // Setup progress bar
+    float progress = float(n_proc_events)/total_events;
+
+    std::string bar = "[";
+    bar += std::string(int(floor(progress*barWidth)), '=');
+    if (bar.size() <= barWidth) bar += ">";
+    if (bar.size() <= barWidth)
+        bar += std::string(int((barWidth+1)-bar.size()), ' ');
+    bar += "] ";
+    std::ostringstream ss;
+    ss.precision(2);
+    ss << std::fixed <<progress*100;
+    bar += ss.str() + " %  ";
+
+
+    std::cout << bar << "(" << passed_events << "/" << n_proc_events << ")" << std::endl;
+}
+
+void Bar::print_trailer()
+{
+    std::cout << "Passed (" << passed_events << ") of total processed (" << n_proc_events << ")" << std::endl;
+}
