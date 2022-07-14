0ed737af53ccba59b03d52da9a30010e2dcfecbd
Dylan Teague <dteague@wisc.edu>
2022-07-14 11:18:03 -0500

    Add new version of met into a new class

---
+++ Analyzer/interface/Met.h
@@ -0,0 +1,39 @@
+#ifndef MET_H_
+#define MET_H_
+
+#include "analysis_suite/Analyzer/interface/Systematic.h"
+#include "analysis_suite/Analyzer/interface/Variable.h"
+
+#include <TTreeReader.h>
+
+enum class MET_Type {
+    PUPPI, PF
+};
+
+class Jet;
+
+class Met : SystematicWeights {
+public:
+    float pt() { return *corr_pt; }
+    float phi() { return *corr_phi; }
+
+    void setup(MET_Type type, TTreeReader& fReader);
+    void setupJEC(Jet& jet);
+    void fix_xy(UInt_t run, int nVertices);
+
+private:
+    TRVariable<float> m_pt;
+    TRVariable<float> m_phi;
+    std::unordered_map<MET_Type, std::string> met_name = {
+        {MET_Type::PUPPI, "PuppiMET"},
+        {MET_Type::PF, "MET"},
+    };
+
+    float *corr_pt, *corr_phi;
+    std::unordered_map<Systematic, std::unordered_map<eVar, float>> m_corr_pt, m_corr_phi;
+    WeightHolder xcorr, ycorr;
+    std::string name;
+};
+
+
+#endif // MET_H_
---
+++ Analyzer/src/Met.cc
@@ -0,0 +1,46 @@
+#include "analysis_suite/Analyzer/interface/Met.h"
+#include "analysis_suite/Analyzer/interface/Jet.h"
+
+void Met::setup(MET_Type type, TTreeReader& fReader)
+{
+    name = met_name[type];
+    m_pt.setup(fReader, (name+"_pt").c_str());
+    m_phi.setup(fReader, (name+"_phi").c_str());
+
+    auto corr_set = getScaleFile("USER", "met_xy");
+
+    xcorr = WeightHolder(corr_set->at("x_correction"));
+    ycorr = WeightHolder(corr_set->at("y_correction"));
+
+    m_corr_pt[Systematic::Nominal] = {{eVar::Nominal, 0}};
+    m_corr_phi[Systematic::Nominal] = {{eVar::Nominal, 0}};
+    for (auto syst : jec_systs) {
+        m_corr_pt[syst] = {{eVar::Up, 0}, {eVar::Down, 0}};
+        m_corr_phi[syst] = {{eVar::Up, 0}, {eVar::Down, 0}};
+    }
+}
+
+void Met::setupJEC(Jet& jet)
+{
+    if (currentSyst == Systematic::Nominal || jet.isJECSyst()) {
+        corr_pt = &m_corr_pt[currentSyst][currentVar];
+        corr_phi = &m_corr_phi[currentSyst][currentVar];
+        auto delta_jet = jet.get_momentum_change();
+        PolarVector met_vec(*m_pt, *m_phi);
+        met_vec -= delta_jet;
+        (*corr_pt) = met_vec.R();
+        (*corr_phi) = met_vec.Phi();
+    } else {
+        corr_pt = &m_corr_pt[Systematic::Nominal][eVar::Nominal];
+        corr_phi = &m_corr_pt[Systematic::Nominal][eVar::Nominal];
+    }
+}
+
+void Met::fix_xy(UInt_t run, int nVertices)
+{
+    float corr_metx = *m_pt*cos(*m_phi)+xcorr.evaluate({name, (float)run, (float)nVertices});
+    float corr_mety = *m_pt*sin(*m_phi)+ycorr.evaluate({name, (float)run, (float)nVertices});
+
+    (*corr_pt) = sqrt(pow(corr_metx, 2) + pow(corr_mety, 2));
+    (*corr_phi) = atan2(corr_mety, corr_metx);
+}
