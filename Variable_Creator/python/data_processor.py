fc8cc798675cdb0544b85f1d8d219cd77a21a184
Dylan Teague <dteague@wisc.edu>
2022-07-21 07:24:52 -0500

    Fix Data processor to make the flat tuples

--- Variable_Creator/python/data_processor.py
+++ Variable_Creator/python/data_processor.py
@@ -1,63 +1,55 @@
 #!/usr/bin/env python3
-from collections import OrderedDict
-import numpy as np
 import pandas as pd
 import logging
-from .vargetter import VarGetter
-import awkward as ak
-from pathlib import Path
 import uproot
-import uproot as upwrite
+
+from .vargetter import VarGetter
+from analysis_suite.commons.info import fileInfo
+from analysis_suite.commons.fake_rate_helper import get_dirnames, get_syst_index
 
 class DataProcessor:
-    def __init__(self, use_vars, systName="Nominal"):
+    def __init__(self, use_vars, lumi, systName="Nominal"):
         """Constructor method
         """
         self.systName = systName
         self.use_vars = use_vars
         self.all_vars = list(use_vars.keys()) + ["scale_factor"]
+        self.lumi = lumi
 
     def get_final_dict(self, directory, tree):
         arr_dict = dict()
-        path = Path(directory)
-        root_files = path.rglob("*.root") if path.is_dir() else [path]
+        root_files = directory.glob("*.root") if directory.is_dir() else [directory]
+        print(tree)
         for root_file in root_files:
-            groups = list()
-            syst = 0
-            with uproot.open(root_file) as f:
-                groups = [key.strip(";1") for key in f.keys() if "/" not in key]
-                systNames = [systName.member("fName") for systName in f[groups[0]]["Systematics"]]
-                systNames = list(OrderedDict.fromkeys(systNames))
-                syst = systNames.index(self.systName)
-                for group in groups:
-                    if tree == "Analyzed" and group == "data":
-                        # blind SR
-                        continue
-                    if group not in arr_dict:
-                        arr_dict[group] = VarGetter(f, tree, group, syst)
-                    else:
-                        arr_dict[group] += VarGetter(f, tree, group, syst)
-        for key, val in arr_dict.items():
-            val.set_JEC(self.systName)
-
+            members = get_dirnames(root_file)
+            syst = get_syst_index(root_file, self.systName)
+            for member in members:
+                if "Signal" in tree and member == "data":
+                    continue # blind SR
+                xsec = fileInfo.get_xsec(member)*self.lumi if member != 'data' else 1
+                vg = VarGetter(root_file, tree, member, xsec, syst)
+                if not vg:
+                    continue
+                vg.setSyst(self.systName)
+                vg.mergeParticles("TightLepton", "TightMuon", "TightElectron")
+                arr_dict[member] = vg
         return arr_dict
 
     def process_year(self, infile, outdir, tree):
         # Process input file
-        arr_dict = self.get_final_dict(infile, tree)
+        vg_dict = self.get_final_dict(infile, tree)
         final_set = dict()
 
-        for sample, arr in arr_dict.items():
-            if not len(arr):
-                logging.warning(f'Sample {sample} has no events in it!')
+        for member, vg in vg_dict.items():
+            if not len(vg):
+                logging.warning(f'Sample {member} has no events in it!')
                 continue
-            df_dict = {varname: func.apply(arr) for varname, func in self.use_vars.items()}
-            df_dict["scale_factor"] = ak.to_numpy(arr.scale)
+            df_dict = {varname: func(vg) for varname, func in self.use_vars.items()}
+            df_dict["scale_factor"] = vg.scale
             df = pd.DataFrame.from_dict(df_dict)
-            final_set[sample] = df
-
-        treename = "" if tree == "Analyzed" else f'_{tree}'
-        self._write_out(outdir / f'processed_{self.systName}{treename}.root', final_set)
+            final_set[member] = df.astype({col: int for col in df.columns if col[0] == 'N'})
+            print(f"Finished setting up {member}")
+        self._write_out(outdir / f'processed_{self.systName}_{tree}.root', final_set)
 
     def _write_out(self, outfile, workSet):
         """**Write out pandas file as a compressed pickle file
@@ -68,10 +60,8 @@ class DataProcessor:
           prediction(pandas.DataFrame): DataFrame of BDT predictions
 
         """
-        branches = {key: np.int32 if key[0] == "N" else  np.float32 for key in self.all_vars}
-        with upwrite.recreate(outfile) as f:
+        with uproot.recreate(outfile) as f:
             for group, df in workSet.items():
                 if not len(df):
                     continue
-                f[group] = upwrite.newtree(branches)
-                f[group].extend(df.to_dict('list'))
+                f[group] = df
--- Variable_Creator/python/job_main.py
+++ Variable_Creator/python/job_main.py
@@ -4,30 +4,30 @@ from pathlib import Path
 import logging
 
 import analysis_suite.commons.configs as config
+from analysis_suite.commons.info import PlotInfo
 from .data_processor import DataProcessor
 import analysis_suite.data.inputs as mva_params
+import analysis_suite.commons.user as user
 
 def setup(cli_args):
     argList = list()
     allSysts = config.get_list_systs(**vars(cli_args))
-    trees = config.get_trees(cli_args.years)
+    workdir = user.workspace_area / cli_args.workdir
 
     for year in cli_args.years:
-        outdir = cli_args.workdir / year
+        outdir = workdir / year
         config.checkOrCreateDir(outdir)
-        infile = Path(f'result_{year}.root')
+        infile = user.hdfs_area / f'workspace/signal_region/{year}'
+        trees = list(config.get_trees(infile))
         for tree in trees:
-            if tree == "Analyzed":
-                for syst in allSysts:
-                    argList.append((infile, outdir, tree, year, syst))
-            else:
-                argList.append((infile, outdir, tree, year, "Nominal"))
+            for syst in allSysts:
+                argList.append((infile, outdir, tree, year, syst))
 
     return argList
         
 
 def run(infile, outdir, tree, year, syst):
-    data = DataProcessor(mva_params.allvar, syst)
+    data = DataProcessor(mva_params.allvar, PlotInfo.lumi[year], syst)
     logging.info(f'Processing year {year} with syst {syst} MC')
     data.process_year(infile, outdir, tree)
 
