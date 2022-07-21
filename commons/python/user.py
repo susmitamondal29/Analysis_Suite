51cf8a31ed027ec35e0440a8014078b05dcabc25
Dylan Teague <dteague@wisc.edu>
2022-07-21 05:00:21 -0500

    Add user mod for storing directories and username

---
+++ commons/python/user.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from pathlib import Path
+
+hdfs_name = 'dteague'
+user_name = Path().owner()
+
+hdfs_area = Path(f'/hdfs/store/user/{hdfs_name}')
+submit_area = Path(f'/nfs_scratch/{user_name}')
+analysis_area = Path(f'{__file__}').parents[2]
+workspace_area = analysis_area / 'workspace'
+if not workspace_area.exists():
+    workspace_area.mkdir()
