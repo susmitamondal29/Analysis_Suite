ec89aa37d4bb0af770320e4b383362d62818fb3a
Dylan Teague <dteague@wisc.edu>
2022-07-22 07:10:59 -0500

    Change analyze to allow for multicore running

--- analyze.py
+++ analyze.py
@@ -3,8 +3,11 @@ import os
 import argparse
 from analysis_suite.commons import FileInfo
 import analysis_suite.commons.configs as configs
-import tarfile
 from pathlib import Path
+import numpy as np
+from multiprocessing import Pool
+import subprocess
+import yaml
 
 import ROOT
 ROOT.gROOT.SetBatch(True)
@@ -42,13 +45,8 @@ def getSumW(infiles):
 
 # Use for file in hdfs area
 def get_info_local(filename):
-    sampleName = filename.split('/')
-    analysisName = sampleName[sampleName.index("user")+2]
-
-    analysis, year, selection = analysisName.split("_")
-    isUL = 'UL' in analysis
-    return {"analysis": analysis, "year": year, "selection": selection,
-            "sampleName": sampleName, "isUL": isUL}
+    year="2016"
+    return {"year": year, "sampleName": "data", "selection": "test"}
 
 def get_info_general(filename):
     sampleName = filename.split('/')
@@ -57,20 +55,42 @@ def get_info_general(filename):
                 "UL16NanoAODv": "2016postVFP",
                 "UL17": "2017",
                 "UL18": "2018",
-                "Run2016" : "2016",
+                "Run2016" : "2016postVFP",
                 "Run2017" : "2017",
                 "Run2018" : "2018",
                 }
     year = None
-    for yearName in yearDict.keys():
+    for yearName, yearkey in yearDict.items():
         if yearName in filename:
-            year = yearDict[yearName]
+            if yearkey == "2016postVFP" and "HIPM" in filename:
+                year = "2016preVFP"
+            else:
+                year = yearkey
             break
 
     isUL = "UL"  in filename
     return {"year": year, "selection": "From_DAS",
-            "sampleName": sampleName, "isUL": isUL}
+            "sampleName": sampleName}
 
+def run_multi(start, evts, files, inputs, selector):
+    fChain = ROOT.TChain()
+    for fname in files:
+        fChain.Add(f"{fname}/Events")
+
+    selector = getattr(ROOT, inputs["MetaData"]["Analysis"])()
+    rInputs = setInputs(inputs)
+
+    with configs.rOpen(f"tmp_{start}.root", "RECREATE") as rOutput:
+        selector.SetInputList(rInputs)
+        selector.setOutputFile(rOutput)
+        fChain.Process(selector, "", evts, start)
+
+        anaFolder = selector.getOutdir()
+        for tree in [tree.tree for tree in selector.getTrees()]:
+            anaFolder.WriteObject(tree, tree.GetTitle())
+        for i in selector.GetOutputList():
+            anaFolder.WriteObject(i, i.GetName())
+    return
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="main")
@@ -78,6 +98,8 @@ if __name__ == "__main__":
     parser.add_argument("-o", "--outfile", default="output.root")
     parser.add_argument("--test", action='store_true')
     parser.add_argument("-v", "--verbose", default=-1)
+    parser.add_argument("-a", "--analysis")
+    parser.add_argument("-j", "--cores", default = 1, type=int)
     args = parser.parse_args()
 
     inputfile = args.infile if (env := os.getenv("INPUT")) is None else env
@@ -89,7 +111,6 @@ if __name__ == "__main__":
         with open(inputfile) as f:
             files = [line.strip() for line in f]
 
-    print(files)
     testfile = files[0]
     if "root://" in testfile and "user" not in testfile:
         details = get_info_general(testfile)
@@ -98,9 +119,12 @@ if __name__ == "__main__":
     info = FileInfo(**details)
     groupName = info.get_group(details["sampleName"])
     datadir = Path(os.getenv("CMSSW_BASE"))/"src"/"analysis_suite"/"data"
-    with open(datadir /".analyze_info") as f:
-        analysis = f.readline().strip()
-    print(groupName)
+
+    if args.analysis:
+        analysis = args.analysis
+    else:
+        with open(datadir /".analyze_info") as f:
+            analysis = f.readline().strip()
 
     # Setup inputs
     inputs = dict()
@@ -112,30 +136,53 @@ if __name__ == "__main__":
         'Year': details["year"],
     }
     if args.test:
-        inputs['MetaData'].update({'Xsec': 1, 'isData': False})
+        inputs['MetaData'].update({'Xsec': 1, 'isData': True})
     else:
         inputs['MetaData'].update({'Xsec': info.get_xsec(groupName), 'isData': info.is_data(groupName)})
     inputs["Verbosity"] = args.verbose
     inputs["Systematics"] = configs.get_shape_systs()
     # Possibly need to fix for fakefactor stuff
     rInputs = setInputs(inputs)
+    if int(args.verbose) > 0:
+        print(yaml.dump(inputs, indent=4, default_flow_style=False))
 
     # Run Selection
     fChain = ROOT.TChain()
     for fname in files:
         fChain.Add(f"{fname}/Events")
 
-    selector = getattr(ROOT, analysis)()
+    nEntries= fChain.GetEntries()
+    job_size = np.ceil(nEntries/args.cores)
+    starts = np.arange(nEntries, step=job_size)
+    steps = job_size*np.ones(args.cores)
+    steps[-1] += nEntries - np.sum(steps)
+    
+    if args.cores == 1:
+        selector = getattr(ROOT, analysis)()
 
-    with configs.rOpen(outputfile, "RECREATE") as rOutput:
-        selector.SetInputList(rInputs)
-        selector.setOutputFile(rOutput)
-        fChain.Process(selector, "")
+        with configs.rOpen(outputfile, "RECREATE") as rOutput:
+            selector.SetInputList(rInputs)
+            selector.setOutputFile(rOutput)
+            fChain.Process(selector, "")
+            ## Output
+            anaFolder = selector.getOutdir()
+            anaFolder.WriteObject(getSumW(files), "sumweight")
+            for tree in [tree.tree for tree in selector.getTrees()]:
+                anaFolder.WriteObject(tree, tree.GetTitle())
+            for i in selector.GetOutputList():
+                anaFolder.WriteObject(i, i.GetName())
+    else:
+        argList = []
+        for start, step in zip(starts, steps):
+            argList.append((int(start), int(step), files, inputs, getattr(ROOT, inputs["MetaData"]["Analysis"])()))
 
-        ## Output
-        anaFolder = selector.getOutdir()
-        anaFolder.WriteObject(getSumW(files), "sumweight")
-        for tree in [tree.tree for tree in selector.getTrees()]:
-            anaFolder.WriteObject(tree, tree.GetTitle())
-        for i in selector.GetOutputList():
-            anaFolder.WriteObject(i, i.GetName())
+        with Pool(args.cores) as pool:
+            pool.starmap(run_multi, argList)
+
+        subprocess.call(f"hadd -f -v 1 {outputfile} tmp*.root", shell=True)
+        with configs.rOpen(outputfile, "UPDATE") as rOutput:
+            anaFolder = rOutput.GetDirectory(groupName)
+            anaFolder.WriteObject(getSumW(files), "sumweight")
+
+        for tmp_file in Path(".").glob("tmp*root"):
+            tmp_file.unlink()
