9e9bf9a2389c427c1987ef86b3cd480b75e3ee2c
Dylan Teague <dteague@wisc.edu>
2022-08-01 07:44:27 -0500

    Fix combine code to work with new format

--- Combine/python/card_maker.py
+++ Combine/python/card_maker.py
@@ -2,17 +2,18 @@
 import numpy as np
 
 class Card_Maker:
-    def __init__(self, path, channels, plot_groups, variable):
+    def __init__(self, path, year, cr, plot_groups, variable):
         self.variable = variable
         self.path = path
-        self.years = channels
-        self.channels = np.core.defchararray.add("yr", np.array(channels))
+        self.year = year
+        self.cr = cr
+        self.channels = np.array([cr])
         self.plot_groups = np.array(plot_groups)
         self.nChans = len(self.channels)
         self.nGroups = len(self.plot_groups)
 
     def __enter__(self):
-        self.f = open(f"{self.path}/{self.variable}_card.txt", 'w')
+        self.f = open(f"{self.path}/{self.variable}_{self.year}_{self.cr}_card.txt", 'w')
         return self
 
     def __exit__(self, type, value, traceback):
@@ -36,7 +37,7 @@ class Card_Maker:
         self.end_section()
 
         # Specify shape locations
-        self.write(f"shapes * * {self.variable}_$CHANNEL.root $PROCESS_Nominal $PROCESS_$SYSTEMATIC")
+        self.write(f"shapes * * {self.variable}_{self.year}_$CHANNEL.root $PROCESS_Nominal $PROCESS_$SYSTEMATIC")
         self.end_section()
 
         # Specify channels and number of events
@@ -45,7 +46,7 @@ class Card_Maker:
         self.end_section()
 
         # Specify channel and plot names with MC counts
-        self.write("bin"+"".join(["\t{}".format(chan)*self.nGroups for chan in self.channels]))
+        self.write("bin"+"".join([f"\t{chan}"*self.nGroups for chan in self.channels]))
         self.write("process"+ self.tab_list(self.plot_groups)*self.nChans)
         self.write("process"+ self.tab_list(np.arange(self.nGroups))*self.nChans)
         self.write("rate" + self.tab_list(-1*np.ones(self.nChans*self.nGroups)))
@@ -53,6 +54,6 @@ class Card_Maker:
 
         # Specify systematics
         for syst in syst_list:
-            self.write(syst.output(self.plot_groups, self.years))
+            self.write(syst.output(self.plot_groups, [self.year]))
         self.write("syst_error group = " + " ".join([syst.name for syst in syst_list]))
         self.write("* autoMCStats 1")
--- Combine/python/job_main.py
+++ Combine/python/job_main.py
@@ -1,55 +1,54 @@
 #!/usr/bin/env python3
-import logging
-from pathlib import Path
-import uproot as upwrite
-import numpy as np
-
-from analysis_suite.commons import GroupInfo, PlotInfo
-from analysis_suite.commons.configs import getGroupDict, get_list_systs, checkOrCreateDir, clean_syst
-from .histogram_creater import getNormedHistos
+import uproot
 
 from .card_maker import Card_Maker
-from .hist_writer import from_boost
 
-import analysis_suite.data.inputs as mva_params
+from analysis_suite.commons import GroupInfo, PlotInfo
+from analysis_suite.commons.configs import get_list_systs, checkOrCreateDir
+from analysis_suite.Plotting.plotter import Plotter
+import analysis_suite.data.inputs as inputs
+from analysis_suite.data.plotInfo.plotInfo import combine
 
 def setup(cli_args):
-    group_info = GroupInfo(mva_params.color_by_group, **vars(cli_args))
-    plot_info = PlotInfo(cli_args.info)
-    allSysts = get_list_systs(**vars(cli_args))
+    group_info = GroupInfo(inputs.color_by_group, **vars(cli_args))
+    allSysts = get_list_systs(cli_args.workdir, cli_args.tool)
 
     workdir = cli_args.workdir / "combine"
     checkOrCreateDir(workdir)
 
-    argList = list()
-    for year in cli_args.years:
-        inpath = cli_args.workdir/year
+    ginfo = GroupInfo(inputs.color_by_group)
+    groups = ginfo.setup_groups()
 
-        argList.append((inpath, workdir, group_info, plot_info, cli_args.fit_var, year, allSysts))
+    argList = list()
+    for cr, graph in combine.items():
+        region, graph = graph
+        for year in cli_args.years:
+            inpath = cli_args.workdir/year
+            outfile = workdir / f'{graph.name}_{year}_{cr}.root'
+            argList.append((inpath, outfile, graph, region, groups, year, allSysts))
 
     return argList
 
-def run(inpath, outpath, file_info, plot_info, histName, year, systs):
-    with upwrite.recreate(outpath / f'{histName}_yr{year}.root') as f:
+
+def run(inpath, outfile, graph, region, groups, year, systs):
+    print(outfile)
+    with uproot.recreate(outfile) as f:
         for syst in systs:
-            groupHists = getNormedHistos(inpath/f"test_{syst}.root", file_info,
-                                         plot_info, histName, year)
+            plotter = Plotter(inpath/f'test_{syst}_{region}.root', groups)
+            plotter.fill_hists(graph)
             syst = syst.replace("_up", "Up").replace("_down", "Down")
             if syst == "Nominal":
-                groupHists["data_obs"] = np.array(list(groupHists.values())).sum()
-            for group, hist in groupHists.items():
-                f[f"{group}_{syst}"] = from_boost(hist.hist, histName)
+                f[f"data_obs_{syst}"] = plotter.get_sum(groups.keys(), graph).hist
+            for group, hist in plotter.get_hists(graph.name).items():
+                f[f"{group}_{syst}"] = hist.hist
 
 
 def cleanup(cli_args):
-    group_info = GroupInfo(mva_params.color_by_group, **vars(cli_args))
-    shapeSysts = {clean_syst(syst) for syst in get_list_systs(**vars(cli_args))}
-    allSysts = [syst for syst in mva_params.systematics
-                if syst.name in shapeSysts or syst.syst_type == "lnN"]
+    workdir = cli_args.workdir / "combine"
+    ginfo = GroupInfo(inputs.color_by_group)
+    groups = ginfo.setup_groups()
 
-    group_list = list(group_info.get_memberMap().keys())
-    group_list.remove(cli_args.signal)
-    group_list.insert(0, cli_args.signal)
-
-    with Card_Maker(cli_args.workdir/"combine", cli_args.years, group_list, cli_args.fit_var) as card:
-        card.write_systematics(allSysts)
+    for cr, graph in combine.items():
+        for year in cli_args.years:
+            with Card_Maker(workdir, year, cr, list(groups.keys()), graph[1].name) as card:
+                card.write_systematics(inputs.systematics)
