8bde92f19510439ef7dea1dfa106374da8ff74f1
Dylan Teague <dteague@wisc.edu>
2022-10-19 06:46:46 -0500

    Remove threetop files from new master branch

--- skim/src/classes.h
+++ skim/src/classes.h
@@ -1,17 +1,7 @@
 #include "analysis_suite/skim/interface/BaseSelector.h"
-#include "analysis_suite/skim/interface/ThreeTop.h"
-#include "analysis_suite/skim/interface/FakeRate.h"
-#include "analysis_suite/skim/interface/Nonprompt_Closure.h"
-#include "analysis_suite/skim/interface/Lepton_MisId_Closure.h"
-#include "analysis_suite/skim/interface/BEfficiency.h"
 
 namespace {
 namespace {
     BaseSelector pBaseSelector;
-    ThreeTop pThreeTop;
-    FakeRate pFakeRate;
-    Nonprompt_Closure pNonprompt_Closure;
-    Closure_MisId pClosure_MisId;
-    BEfficiency pBEfficiency;
 } // namespace
 } // namespace
